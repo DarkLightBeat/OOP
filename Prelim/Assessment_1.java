e01f1d7bdcb64944f9f82c3bd78f6d82d6e9d566
Kenneth Villabroza <153878111+DarkLightBeat@users.noreply.github.com>
2024-11-27 17:48:22 +0800

    Create Assessment_1.java

---
+++ Prelim/Assessment_1.java
@@ -0,0 +1,59 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name: Kenneth S. Villabroza");
+        System.out.println("Course: CCMS - BS Computer Science");
+        System.out.println("Year Level: 2nd Year");
+        System.out.println();
+        System.out.println("Artist: Chen from EXO & Punch");
+        System.out.println("Writers: Dong Woon Song / Ji Hoon / Rocoberry / Tai Seok Ahn / Young Soo Cho\n");
+        
+        System.out.println("Lyrics:");
+        System.out.println("\tOh, every time I see you");
+        System.out.println("\t그대 눈을 볼 때면 자꾸 가슴이 또 설레여와");
+        System.out.println("\t내 운명이죠 세상 끝이라도");
+        System.out.println("\t지켜주고 싶은 단 한 사람");
+        System.out.println("\tBaby, oh oh oh oh");
+        System.out.println("\tOh oh oh oh");
+        System.out.println("\tBaby, oh oh oh oh\n");
+
+        System.out.println("\tOh, every time I see you");
+        System.out.println("\t그대 눈을 볼 때면 자꾸 가슴이 또 설레여와");
+        System.out.println("\t내 운명이죠 세상 끝이라도");
+        System.out.println("\t지켜주고 싶은 단 한 사람\n");
+        
+        System.out.println("\t그대 나를 바라볼 때");
+        System.out.println("\t나를 보며 미소 질 때 난");
+        System.out.println("\t심장이 멈출 것 같아요 난");
+        System.out.println("\t그댄 어떤가요");
+        System.out.println("\t난 정말 감당하기 힘든걸");
+        System.out.println("\t온종일 그대 생각해\n");
+        
+        System.out.println("\t조금 멀리 우리 돌아왔지만");
+        System.out.println("\t지금이라도 난 괜찮아\n");
+        
+        System.out.println("\tOh, every time I see you");
+        System.out.println("\t그대 눈을 볼 때면 자꾸 가슴이 또 설레여와");
+        System.out.println("\t내 운명이죠 세상 끝이라도");
+        System.out.println("\t지켜주고 싶은 단 한 사람\n");
+        
+        System.out.println("\t날 떠나지 말아요");
+        System.out.println("\t가끔은 알 수 없는 미래라 해도");
+        System.out.println("\t날 믿고 기다려줄래요\n");
+        
+        System.out.println("\t워 나만의 그대여");
+        System.out.println("\t내겐 전부라는 말");
+        System.out.println("\t고백한 적이 있었나요\n");
+        
+        System.out.println("\t내 운명이죠 세상 끝이라도 지켜주고 싶은 너\n");
+        
+        System.out.println("\tBaby oh oh oh oh 사랑할래요");
+        System.out.println("\tOh oh oh oh");
+        System.out.println("\t네 눈빛과 네 미소와 그 향기까지도");
+        System.out.println("\tBaby oh oh oh oh\n");
+        
+        System.out.println("\t기억해줘요");
+        System.out.println("\tOh oh oh oh");
+        System.out.println("\t언제나 우리 함께 있음을");
+        System.out.println("\tI love you");
+    }
+}
